ae0a8af7b8ea1dab19f69cfb09cbfd504e5d6dff
mustafa99mm <am.ss09921@gmail.com>
2026-06-23 04:59:34 +0300

    Update URL for API request in script.py

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import requests
 
-url = "https://asurascans.com/api/chapter/sword-gods-livestream/23"
-
+url = "https://api.asurascans.com/api/reader/6067/chapter/259518"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
     "Referer": "https://asurascans.com/",
