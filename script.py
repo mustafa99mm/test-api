de042279b719d5988dd1779f3b6ccf3ada74078e
mustafa99mm <am.ss09921@gmail.com>
2026-06-23 04:55:32 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-url = "https://gg.asuracomic.net/api/reader/6067/chapter/259518"
-
+url = "https://asurascans.com/api/chapter/sword-gods-livestream/23"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
     "Referer": "https://asuracomic.net/",
